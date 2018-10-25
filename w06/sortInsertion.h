7198557e6af905ffd17bb506eb44f9b17a114f3f
Cameron D. Dockstader <cameron.dockstader@gmail.com>
2018-10-25 13:42:36 -0600

    Misc cleanup

--- w06/sortInsertion.h
+++ w06/sortInsertion.h
@@ -20,52 +20,54 @@
 template <class T>
 void sortInsertion(T array[], int num)
 {
-	// setup the pNode variable, and initialize it to the first item in the array.
 	Node<T> * pNode = new Node<T>;
 	pNode->data = array[0];
-	// starting at the second item, go over the array
+	
 	for (int i = 1; i < num; i++)
 	{
-		// create a new node each loop
 		Node<T> * thisNode = new Node<T>(array[i]);
 
-		if (thisNode->data >= pNode->data) {
-			// while thisnode is larger and we have a next node cycle pNode
+		if (thisNode->data >= pNode->data) 
+		{
 			while (pNode->pNext && thisNode->data > pNode->pNext->data)
 			{
 				pNode = pNode->pNext;
 			}
-			// loop breaks we have the node we need to bump over.
+	
 			thisNode->pNext = pNode->pNext;
 			thisNode->pPrev = pNode;
+
 			if (pNode->pNext)
 			{
 				pNode->pNext->pPrev = thisNode;
 			}
+
 			pNode->pNext = thisNode;
 		}
-		else {
-			// while thisnode is smaller and we have a next node cycle pNde
+		else 
+		{
 			while (pNode->pPrev && thisNode->data < pNode->pPrev->data)
 			{
 				pNode = pNode->pPrev;
 			}
-			// loop breaks we have the node we need to bump over.
-			thisNode->pNext = pNode; // set thisNodes next to pnode
-			thisNode->pPrev = pNode->pPrev; // set thisNodes prev to pNodes prev
+			
+			thisNode->pNext = pNode;
+			thisNode->pPrev = pNode->pPrev;
+
 			if (pNode->pPrev)
 			{
 				pNode->pPrev->pNext = thisNode;
 			}
-			pNode->pPrev = thisNode; // set pNode prev to thisNOde
+
+			pNode->pPrev = thisNode;
 		}
 	}
-	// Get back to the first node (this could probably be improved...
+	
 	while (pNode->pPrev)
 	{
 		pNode = pNode->pPrev;
 	}
-	// Copy the sorted data back in.
+	
 	int iter = 0;
 	while (pNode->pNext)
 	{
@@ -73,7 +75,7 @@ void sortInsertion(T array[], int num)
 		pNode = pNode->pNext;
 		iter++;
 	}
-	// Make sure to grab that last element.
+	
 	array[iter] = pNode->data;
 }
 
