2903b8125b3de3ab8975b0374e4fc88700cd0dc9
Cameron D. Dockstader <cameron.dockstader@gmail.com>
2018-10-28 08:10:05 -0600

    Working on test 1

--- w07/list.h
+++ w07/list.h
@@ -9,14 +9,14 @@ namespace custom
 	class list
 	{
 	private:
-		Node<T> pHead;
-		Node<T> pTail;
+		Node<T> * pHead;
+		Node<T> * pTail;
 		int numElements;
 
 	public:
-		list();
+		list() : pHead(nullptr), pTail(nullptr), numElements(0) {};
 		list(const list &rhs);
-		list &operartor= (const list &rhs);
+		list &operator= (const list &rhs);
 		~list();
 
 		int size() const { return numElements; }
@@ -48,76 +48,73 @@ namespace custom
 		iterator end();
 	};
 	template<class T>
-	inline list<T>::list()
+	list<T>::list(const list &rhs)
 	{
 		//todo
 	}
 	template<class T>
-	inline list<T>::list(const list & rhs)
+	list<T> &list<T>::operator=(const list &rhs)
+	{
+		//todo
+		return *this;
+	}
+	template<class T>
+	list<T>::~list()
 	{
 		//todo
 	}
 	template<class T>
-	inline list<T>::~list()
+	void list<T>::clear()
 	{
 		//todo
 	}
 	template<class T>
-	inline void list<T>::clear()
+	void list<T>::push_back(T t)
 	{
 		//todo
 	}
 	template<class T>
-	inline void list<T>::push_back(T t)
+	void list<T>::push_front(T t)
 	{
 		//todo
 	}
 	template<class T>
-	inline void list<T>::push_front(T t)
+	void list<T>::pop_back()
 	{
 		//todo
 	}
 	template<class T>
-	inline void list<T>::pop_back()
+	void list<T>::pop_front()
 	{
 		//todo
 	}
 	template<class T>
-	inline void list<T>::pop_front()
+	T &list<T>::front()
+	{
+		return pHead->data;
+	}
+	template<class T>
+	const T &list<T>::front() const
+	{
+		return pHead->data;
+	}
+	template<class T>
+	T &list<T>::back()
+	{
+		return pTail->data;
+	}
+	template<class T>
+	const T &list<T>::back() const
+	{
+		return pTail->data;
+	}
+	template<class T>
+	void list<T>::erase(iterator &it)
 	{
 		//todo
 	}
 	template<class T>
-	inline T & list<T>::front()
-	{
-		//todo
-		return T();
-	}
-	template<class T>
-	inline const T & list<T>::front() const
-	{
-		//todo
-		return T();
-	}
-	template<class T>
-	inline T & list<T>::back()
-	{
-		//todo
-		return T();
-	}
-	template<class T>
-	inline const T & list<T>::back() const
-	{
-		//todo
-		return T();
-	}
-	template<class T>
-	inline void list<T>::erase(iterator & it)
-	{
-		//todo
-	}
-	template<class T>
-	inline void list<T>::insert(iterator it, T t)
+	void list<T>::insert(iterator it, T t)
 	{
 		//todo
 	}
