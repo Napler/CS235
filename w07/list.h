77f078bb7d1064ba57ad79b02debad0282e5bd01
Cameron D. Dockstader <cameron.dockstader@gmail.com>
2018-10-28 08:20:29 -0600

    Replacing node class with struct, getting closer to test 1

--- w07/list.h
+++ w07/list.h
@@ -1,10 +1,18 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include "node.h";
-
 namespace custom
 {
+	template<typename T>
+	struct Node
+	{
+		T data;
+		Node<T> *pNext;
+		Node<T> *pPrev;
+
+		Node(T t) : data(t), pNext(nullptr), pPrev(nullptr) {};
+	};
+
 	template <class T>
 	class list
 	{
@@ -12,6 +20,8 @@ namespace custom
 		Node<T> * pHead;
 		Node<T> * pTail;
 		int numElements;
+
+		void linkNodes(Node<T> * front, Node<T> * back);
 
 	public:
 		list() : pHead(nullptr), pTail(nullptr), numElements(0) {};
@@ -47,72 +57,120 @@ namespace custom
 		iterator begin();
 		iterator end();
 	};
+
+
+	template<class T>
+	void list<T>::linkNodes(Node<T> * front, Node<T> * back)
+	{
+		front->pNext = back;
+		back->pPrev = front;
+	}
+
 	template<class T>
 	list<T>::list(const list &rhs)
 	{
 		//todo
 	}
+
 	template<class T>
 	list<T> &list<T>::operator=(const list &rhs)
 	{
 		//todo
 		return *this;
 	}
+
 	template<class T>
 	list<T>::~list()
 	{
 		//todo
 	}
+
 	template<class T>
 	void list<T>::clear()
 	{
 		//todo
 	}
+
 	template<class T>
 	void list<T>::push_back(T t)
 	{
-		//todo
+		Node<T> * newNode = new Node<T>(t);
+
+		if (pHead == NULL)
+		{
+			pHead = newNode;
+			pTail = newNode;
+		}
+		else
+		{
+			linkNodes(pTail, newNode);
+			pTail = newNode;
+		}
+
+		numElements++;
 	}
+
 	template<class T>
 	void list<T>::push_front(T t)
 	{
-		//todo
+		Node<T> * newNode = new Node<T>(t);
+		
+		if (pHead == NULL)
+		{
+			pHead = newNode;
+			pTail = newNode;
+		}
+		else
+		{
+			linkNodes(newNode, pHead);
+			pHead = newNode;
+		}
+		
+		numElements++;
 	}
+
 	template<class T>
 	void list<T>::pop_back()
 	{
 		//todo
 	}
+
 	template<class T>
 	void list<T>::pop_front()
 	{
 		//todo
 	}
+
 	template<class T>
 	T &list<T>::front()
 	{
 		return pHead->data;
 	}
+
 	template<class T>
 	const T &list<T>::front() const
 	{
 		return pHead->data;
 	}
+
 	template<class T>
 	T &list<T>::back()
 	{
 		return pTail->data;
 	}
+
 	template<class T>
 	const T &list<T>::back() const
 	{
 		return pTail->data;
 	}
+
 	template<class T>
 	void list<T>::erase(iterator &it)
 	{
 		//todo
 	}
+
 	template<class T>
 	void list<T>::insert(iterator it, T t)
 	{
