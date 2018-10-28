04365fbb76d2f52fd367ddb92ea421cc5388dc17
Cameron D. Dockstader <cameron.dockstader@gmail.com>
2018-10-28 07:28:26 -0600

    Adding structure, able to build

---
+++ w07/list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "node.h";
+
+namespace custom
+{
+	template <class T>
+	class list
+	{
+	private:
+		Node<T> pHead;
+		Node<T> pTail;
+		int numElements;
+
+	public:
+
+	};
+}
+
+#endif
