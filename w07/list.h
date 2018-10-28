93d73fbca8a18e41a10e38e1c803582ae2081a1d
Cameron D. Dockstader <cameron.dockstader@gmail.com>
2018-10-28 07:39:16 -0600

    Filing out list.h

--- w07/list.h
+++ w07/list.h
@@ -14,7 +14,38 @@ namespace custom
 		int numElements;
 
 	public:
+		list();
+		list(const list &rhs);
+		list &operartor= (const list &rhs);
+		~list();
 
+		int size() const;
+		bool empty() const;
+		void clear();
+
+		void push_back(T t);
+		void push_front(T t);
+		void pop_back();
+		void pop_front();
+
+		T &front();
+		const T &front() const;
+		T &back();
+		const T &back() const;
+
+		// iterators
+		class iterator;
+		class const_iterator;
+		class reverse_iterator;
+		class const_reverse_iterator;
+
+		iterator find(T t);
+
+		void erase(iterator &it);
+		void insert(iterator it, T t);
+
+		iterator begin();
+		iterator end();
 	};
 }
 
